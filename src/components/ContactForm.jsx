b5192da79e5a2e10ac95369d296d668924cc4cea
boby-front <delanglebob38@gmail.com>
2023-10-20 08:01:42 +0200

    add mail

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,6 +1,6 @@
 import React, { useRef, useState } from "react";
 import { motion } from "framer-motion";
-import emailjs from "emailjs-com";
+import emailjs from "@emailjs/browser";
 
 const ConstForm = () => {
   const [sendMessage, setSendMessage] = useState(false);
